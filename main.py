0a4e38ddd8bccef604b992e7af7241196d677358
fumakato <k20035kk@aitech.ac.jp>
2021-12-09 16:09:33 +0900

    日記追加

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from diaries.DiarySample import DiarySample
-from diaries.NagataniDiary import NagataniDiary
+from diaries.FumaDaiary import FumaDaiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [
   DiarySample(),
-  NagataniDiary(),
+  FumaDaiary()
 ] 
 
 for d in diaries:
